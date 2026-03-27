ab71cc15ac87bc241d997567c139536499a8b927
Anik Baul <anikbaul2@gmail.com>
2026-03-28 02:00:48 +0530

    Login Registration Routes Create

--- server/src/routes/v1/authroutes.routes.js
+++ server/src/routes/v1/authroutes.routes.js
@@ -1,3 +1,33 @@
-import { _v1Router } from './index.js';
+import express from 'express';
+import { registerStudent } from '../../controller/Student/auth.controller.js';
+import { registerFaculty } from '../../controller/Faculty/auth.controller.js';
+import { registerAdmin } from '../../controller/Admin/auth.controller.js';
+import { loginStudent } from '../../controller/Student/auth.controller.js';
+import { loginFaculty } from '../../controller/Faculty/auth.controller.js';
+import { loginAdmin } from '../../controller/Admin/auth.controller.js';
 
+const authRouter = express.Router();
 
+// Register
+
+// api/v1/auth/student/register
+authRouter.post('/student/register', registerStudent);
+
+// api/v1/auth/faculty/register
+authRouter.post('/faculty/register', registerFaculty);
+
+// api/v1/auth/admin/register
+authRouter.post('/admin/register', registerAdmin);
+
+// Login
+
+// api/v1/auth/student/login
+authRouter.post('/student/login', loginStudent);
+
+// api/v1/auth/faculty/login
+authRouter.post('/faculty/login', loginFaculty);
+
+// api/v1/auth/admin/login
+authRouter.post('/admin/login', loginAdmin);
+
+export default authRouter;
